4119e19cbd911a5ca37377087dbf97c84d02bc14
Xiao, Huang <xh0217@gmail.com>
2016-11-19 16:09:00 +0100

    Site updated 2016-11-19 16:08:59

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,11 @@
+// window.onload = function() {
+//   Particles.init({
+//     selector: '#myCanvas',
+//     color: '#0047AB',
+//     connectParticles: true
+//   });
+// };
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function toggleNav() {
     var x = document.getElementById("myTopnav");
