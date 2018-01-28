550ff7e7e04adce13d8223a1d1a7f6b941d07cb1
Xiao, Huang <xiaohu@in.tum.de>
2018-01-28 20:33:38 +0100

    Site updated 2018-01-28 20:33:37

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,31 @@
-window.onload = function() {
-    $('.game').blockrain();  
+window.onload = function () {
+    config = {
+        autoplay: false, // Let a bot play the game
+        autoplayRestart: true, // Restart the game automatically once a bot loses
+        showFieldOnStart: true, // Show a bunch of random blocks on the start screen (it looks nice)
+        theme: 'gameboy', // The theme name or a theme object
+        blockWidth: 10, // How many blocks wide the field is (The standard is 10 blocks)
+        autoBlockWidth: false, // The blockWidth is dinamically calculated based on the autoBlockSize. Disabled blockWidth. Useful for responsive backgrounds
+        autoBlockSize: 12, // The max size of a block for autowidth mode
+        difficulty: 'normal', // Difficulty (normal|nice|evil).
+        speed: 20, // The speed of the game. The higher, the faster the pieces go.
+      
+        // Copy
+        playText: 'Let\'s play some Tetris',
+        playButtonText: 'Play',
+        gameOverText: 'Game Over',
+        restartButtonText: 'Play Again',
+        scoreText: 'Score',
+      
+        // Basic Callbacks
+        onStart: function(){},
+        onRestart: function(){},
+        onGameOver: function(score){},
+      
+        // When a line is made. Returns the number of lines, score assigned and total score
+        onLine: function(lines, scoreIncrement, score){}
+      }
+    $('.game').blockrain(config);  
   };
   
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
